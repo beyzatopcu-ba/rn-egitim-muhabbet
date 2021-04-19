9cd0d266b0886bf2e6bc0626b2b6f475e0b054b7
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-04-19 13:43:56 +0300

    styling files

---
+++ src/StylingConstants/Fonts.js
@@ -0,0 +1,14 @@
+import Metrics from './Metrics';
+
+const type = {
+};
+
+const step = Metrics.width * 0.0025;
+const zero = Metrics.width * 0.002;
+
+const size = punto => zero + step * punto;
+
+export default {
+  type,
+  size,
+};
