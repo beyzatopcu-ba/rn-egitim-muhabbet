4e412c0b27b06cacef98020e7238636d220ae740
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-04-19 15:26:37 +0300

    styles object changed to function

--- src/Modules/Settings/styles/SettingsScreenStyles.js
+++ src/Modules/Settings/styles/SettingsScreenStyles.js
@@ -1,5 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { cn } from '../../Theming';
 
-export default StyleSheet.create({
-    
+export default Colors => StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors[cn.settings.background],
+    }
 });
