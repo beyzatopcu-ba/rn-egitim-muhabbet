5e5bb6f8b74c14416f1fbc8cc87bfa12589e2ce9
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-04-19 16:20:52 +0300

    updating theme 2

--- src/Modules/Theming/index.js
+++ src/Modules/Theming/index.js
@@ -1,3 +1,3 @@
 export { ThemeReducer, THEMING_NAMESPACE } from './Redux/ThemingRedux';
 export { cn } from './Colors';
-export { useTheme, useThemedColors, useThemedStyles, useThemedValues } from './ThemingHooks';
+export { useTheme, useThemedColors, useThemedStyles, useThemedValues, useChangeTheme } from './ThemingHooks';
