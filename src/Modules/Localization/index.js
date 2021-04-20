46bac73cadcdd162d90d968d20e06ce64bc736df
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-04-20 11:01:35 +0300

    started integrating localization

---
+++ src/Modules/Localization/index.js
@@ -0,0 +1,5 @@
+export { LocalizationReducer, LOCALIZATION_NAMESPACE } from './LocalizationRedux';
+export { tn } from './Texts';
+import I18n from './I18nConfig';
+
+export const loc = I18n;
--- src/Redux/Reducers.js
+++ src/Redux/Reducers.js
@@ -1,11 +1,13 @@
 // Packages
 import { combineReducers } from 'redux';
 import { ThemeReducer, THEMING_NAMESPACE } from '../Modules/Theming';
+import { LocalizationReducer, LOCALIZATION_NAMESPACE } from '../Modules/Localization';
 
 // Reducers
 
 export default combineReducers({
-    [THEMING_NAMESPACE]: ThemeReducer
+    [THEMING_NAMESPACE]: ThemeReducer,
+    [LOCALIZATION_NAMESPACE]: LocalizationReducer,
 });
 
 
