1a663649d10868e11ed0e5277d104174ce375e78
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-04-20 13:30:15 +0300

    auth navigation added

--- src/Navigation/MainNavigation.js
+++ src/Navigation/MainNavigation.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigation from './AppNavigation';
+import AuthNavigation from './AuthNavigation';
 
 const MainNavigation = (props) => {
+    const user = null;
     return (
         <>
             <StatusBar barStyle={"light-content"}/>
             <NavigationContainer>
-                <AppNavigation />
+                {
+                    user === null ?
+                    <AuthNavigation />
+                    :
+                    <AppNavigation />
+                }
             </NavigationContainer>
         </>
     );
