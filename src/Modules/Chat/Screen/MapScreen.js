d21f8cb70588e315c6519daab1270bb4a313f214
beyzatopcu-ba <beyza.topcu@bilgeadam.com>
2021-05-10 23:21:53 +0300

    style changes and maps testing

--- src/Modules/Chat/Screen/MapScreen.js
+++ src/Modules/Chat/Screen/MapScreen.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 import Icon from '../../../Components/Icon';
 import { Svgs } from '../../../StylingConstants';
@@ -21,19 +22,21 @@ const MapScreen = props => {
     return (
 
         <View style={{ flex: 1 }}>
-            <View style={styles.container}>
-                <SafeAreaView style={{ flex: 1 }}>
-                    <View style={{ flex: 1 }}>
-                        <TouchableOpacity onPress={props.navigation.goBack} style={styles.backTouchable}>
-                            <Text style={styles.backText}>{"<"}</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.iconContainer} onPress={_onPress_Send}>
-                            <Icon svg={Svgs.Send} iconStyle={{ color: colors[cn.chat.sendIcon] }} />
-                        </TouchableOpacity>
-                    </View>
-
-                </SafeAreaView>
-            </View>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
+            <TouchableOpacity onPress={props.navigation.goBack} style={styles.backTouchable}>
+                <Text style={styles.backText}>{"<"}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.iconContainer} onPress={_onPress_Send}>
+                <Icon svg={Svgs.Send} iconStyle={{ color: colors[cn.chat.sendIcon] }} />
+            </TouchableOpacity>
         </View>
     );
 };
